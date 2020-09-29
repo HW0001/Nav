8f3097619ef794a86a91447b8a236c78b1e7ab2b
HW0001 <1600860624@qq.com>
2020-09-29 16:07:50 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
- let items = window.localStorage.getItem("navSite")
- console.log(items)
+ let items = window.localStorage.getItem("navSite") 
  if(!items)
  { 
-    let  items = [{
+        items = [{
         log:"A",
         logType:'text',
         url:"https://www.acfun.cn/",
@@ -14,12 +13,13 @@
         alias:"bilibili"
      }
     ] 
-    window.localStorage.setItem("navSite",JSON.stringify(items))
+    saveLocltionSession()
 }
 else{ 
     items=JSON.parse(items)
 }
 buildCard()
+
 function buildCard(){
 $("ol>li:not(#last)").remove()
 items.forEach(node=>{
@@ -32,6 +32,12 @@ items.forEach(node=>{
 `).insertBefore($("#last"))
 }) 
 }
+
+function saveLocltionSession(){
+    window.localStorage.setItem("navSite",JSON.stringify(items))
+}
+
+
  $("#addsite").on("click",function(){
    let url = prompt("请输入添加网址")
    if(!url) return 
@@ -40,12 +46,13 @@ items.forEach(node=>{
    } 
    var b = url.match(/http:\/\/w*\.*(\w*)/)
    var obj={
-    log:b[1][0],
+    log:b[1][0].toUpperCase(),
     logType:'text',
-    url:"https://www.acfun.cn/",
+    url:url,
     alias:b[1]
    }
-   items.push(obj)
-   window.localStorage.setItem("navSite",JSON.stringify(items))
+   items.push(obj) 
+   saveLocltionSession()
    buildCard()
 })
+ 
